3fad12a2a1d9c92dba8a29ba5202dfebf3db6457
David <david@mju.io>
2020-11-27 19:11:27 -0800

    Starting this over... Enough exploring. Make some stuff worth now.

--- src/bytes/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod reader;
-
-pub static CRLF: &'static [u8] = &[0x0d, 0x0a];
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,2 @@
-pub mod electrical;
-pub mod flow;
-
 fn main() {
-    electrical::mppt::run();
-    
-    let f = flow::Flow::new()
-        .to(|flow| flow.source())
-        .to(|flow| flow.source());
 }

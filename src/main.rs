9b9a72f6d706a627bc24828586ab1151cc3371ff
David Kern <david@mju.io>
2020-12-04 15:34:58 -0800

    Implement keep alive heartbeat and move socket to module

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,39 @@
+#[macro_use]
+extern crate log;
+
 use actix_web::{web, App, Error, HttpServer, middleware, HttpRequest, HttpResponse};
 use actix_web_actors::ws;
 use actix_files::Files;
-use actix::{Actor, StreamHandler};
+use actix::{Actor, ActorContext, AsyncContext, StreamHandler};
+use std::time::{Instant, Duration};
 
 pub mod phy;
+pub mod ui;
+
+use ui::socket::UISocket;
 
 #[cfg(test)]
 mod test;  // Test fixtures
 
-/// Actor handling client websocket
-struct ClientSocket;
-
-impl Actor for ClientSocket {
-    type Context = ws::WebsocketContext<Self>;
-}
-
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ClientSocket {
-    fn handle(
-        &mut self,
-        msg: Result<ws::Message, ws::ProtocolError>,
-        ctx: &mut Self::Context,
-    ) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
-        }
-    }
-}
-
 /// handle websocket handshake and spawn `ClientSocket` actor
 async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let resp = ws::start(ClientSocket {}, &req, stream);
-    println!("{:?}", resp);
+    let resp = UISocket::start(req, stream);
+    debug!("ws_index response: {:?}", resp);
     resp
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    std::env::set_var("RUST_LOG", "habctl=debug,actix_server=info,actix_web=info");
     env_logger::init();
 
+    debug!("creating HttpServer");
     HttpServer::new(|| {
         App::new()
             // enable logger
             .wrap(middleware::Logger::default())
-            // websocket route
-            .service(web::resource("/ws/").route(web::get().to(ws_index)))
+            // user interface websocket route
+            .service(web::resource("/socket/ui").route(web::get().to(ws_index)))
             // static files
             .service(Files::new("/", "../habux/dist/").index_file("index.html"))
     })
---
+++ src/ui/socket.rs
@@ -0,0 +1,74 @@
+use actix_web::{web, App, Error, HttpServer, middleware, HttpRequest, HttpResponse};
+use actix_web_actors::ws;
+use actix_files::Files;
+use actix::{Actor, ActorContext, AsyncContext, StreamHandler};
+use std::time::{Instant, Duration};
+
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(10);
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(20);
+
+/// Actor handling user interface websocket
+#[derive(Debug)]
+pub struct UISocket {
+    id: u64,
+    last_heartbeat: Instant,
+}
+
+impl UISocket {
+    pub fn start(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+        ws::start(
+            Self {
+                id: 0,
+                last_heartbeat: Instant::now(),
+            },
+            &req,
+            stream)
+    }
+
+    fn heartbeat(&self, ctx: &mut <Self as Actor>::Context) {
+        let id = self.id;
+        ctx.run_interval(HEARTBEAT_INTERVAL, move |actor, ctx| {
+            if Instant::now().duration_since(actor.last_heartbeat) > CLIENT_TIMEOUT {
+                info!("UISocket[{}] heartbeat missed, disconnecting", id);
+                ctx.stop();
+            } else {
+                ctx.ping(b"");
+            }
+        });
+    }
+}
+
+impl Actor for UISocket {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.heartbeat(ctx);
+    }
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for UISocket {
+    fn handle(
+        &mut self,
+        msg: Result<ws::Message, ws::ProtocolError>,
+        ctx: &mut Self::Context,
+    ) {
+        debug!("UISocket[{}] => {:?}", self.id, msg);
+        match msg {
+            Ok(ws::Message::Ping(msg)) => {
+                self.last_heartbeat = Instant::now();
+                ctx.pong(&msg)
+            },
+            Ok(ws::Message::Pong(_)) => {
+                self.last_heartbeat = Instant::now();
+            },
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(reason)) => {
+                ctx.close(reason);
+                ctx.stop();
+            }
+            _ => ctx.stop(),
+        }
+    }
+}

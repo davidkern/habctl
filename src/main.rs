8c4747aa37c7dd1abbd9f409ec838e8e170c715b
David Kern <david@mju.io>
2020-12-06 17:52:48 -0800

    Warp Hello world with tokio 0.3 compat

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use warp::Filter;
+use tokio_compat_02::FutureExt;
+
 pub mod hardware;
 pub mod network;
 pub mod telemetry;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([0, 0, 0, 0], 8080))
+        .compat() // remove when warp is tokio 0.3
+        .await;
 }

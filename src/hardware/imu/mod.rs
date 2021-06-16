cbe24317404ffb8f2da1a7c25e3a6d6ad80e1ac4
David Kern <david@mju.io>
2021-06-15 18:10:29 -0700

    get chip id

--- src/hardware/imu/mod.rs
+++ src/hardware/imu/mod.rs
@@ -4,6 +4,9 @@ use serde::Serialize;
 use std::sync::{Arc, Mutex};
 use crate::hardware::device::Device;
 use std::time::SystemTime;
+use tokio::task;
+use nalgebra as na;
+use std::os::unix::io::AsRawFd;
 
 #[derive(Serialize)]
 pub struct Icm20948 {
@@ -39,14 +42,47 @@ impl Icm20948 {
             log::debug!("Icm20948 {} is in loopback mode.", self.name);
             sleep(Duration::from_secs(600)).await;
         } else {
-
+            task::block_in_place(|| {
+                self.stream_i2c_data();
+            })
         }
 
         Ok(())
     }
+
+    fn stream_i2c_data(&self) {
+        let mut frame = ImuFrame::default();
+
+        if let Ok(i2c) = i2c_linux::I2c::from_path(self.port.to_owned()) {
+            let mut i2c = i2c;
+            // i2c mode 8 (0-axis mode)
+            let mut who_am_i: [u8; 1] = [0u8];
+            let result = i2c.i2c_read_block_data(0x00, &mut who_am_i);
+            frame.temperature = Some(who_am_i[0].into());
+            *self.telemetry.lock().unwrap() = frame;
+
+            loop {
+                std::thread::sleep(std::time::Duration::from_secs(1));
+            }
+        }
+
+    }    
 }
+
 
 #[derive(Default, Clone, Debug, Serialize)]
 pub struct ImuFrame {
     timestamp: Option<SystemTime>,
+
+    /// Rotation rate in degress per second (max 2000 dps)
+    gyroscope: Option<na::Rotation3<f32>>,
+
+    /// Accelerameter 3-vector in g (max 2g)
+    accelerometer: Option<na::Vector3<f32>>,
+
+    /// Magnetometer 3-vector in Tesla (max 4900 microTesla)
+    magnetometer: Option<na::Vector3<f32>>,
+
+    /// IMU temperature in deg C
+    temperature: Option<u16>,
 }
